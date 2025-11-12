ceb6cfd6e80ec3906e4388133a51cc26aefe9cb9
DraganBar <151796796+DraganBar@users.noreply.github.com>
2025-11-12 21:14:08 +0100

    Update Zivotinja.cs

--- Models/Zivotinja.cs
+++ Models/Zivotinja.cs
@@ -1,19 +1,1 @@
-namespace Zoo.Models
-{
-    public class Zivotinja
-    {
-        public int Id { get; set; }
-        public string Ime { get; set; } = string.Empty;
-        public string? LatinskiNaziv { get; set; }
-        public string? HrvatskiNaziv { get; set; }
-        public string? NacinNabave { get; set; }
-        public DateTime? DatumNabave { get; set; }
-        public bool? Aktivna { get; set; }
 
-        public int NastambaId { get; set; }
-        public Nastamba? Nastamba { get; set; }
-
-        public ICollection<Trosak> Troskovi { get; set; } = new List<Trosak>();
-        public ICollection<IncidentZivotinja> Incidenti { get; set; } = new List<IncidentZivotinja>();
-    }
-}
